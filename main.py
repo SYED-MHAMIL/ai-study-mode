7975254c6330a5572684ca49291bbc227a8c63fd
syedmhamil <syedmahamil56@gmail.com>
2025-11-09 18:30:22 +0500

    intializa

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from agents.run import RunConfig
 # Load the environment variables from the .env file
 load_dotenv()
 
-gemini_api_key = os.getenv("GEMINI_API_KEY")
+gemini_api_key = os.getenv("GOOGLE_API_KEY")
 
 # Check if the API key is present; if not, raise an error
 if not gemini_api_key:
