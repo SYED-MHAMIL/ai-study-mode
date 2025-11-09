98b0ab99c2d9948e1b326f764836e8b4a4ac5aca
syedmhamil <syedmahamil56@gmail.com>
2025-11-09 18:21:30 +0500

    intializa

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from typing import cast
+import chainlit as cl
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+
+
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+
+
+
+@cl.on_chat_start
+async def start():
+    #Reference: https://ai.google.dev/gemini-api/docs/openai
+    external_client = AsyncOpenAI(
+        api_key=gemini_api_key,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    )
+
+    model = OpenAIChatCompletionsModel(
+        model="gemini-2.0-flash",
+        openai_client=external_client
+    )
+    
+    
+
+    await cl.Message(content="Hello Baby").send()
+
+
+def main():
+    print("Hello from openai-study-mode!")
+
+
+if __name__ == "__main__":
+    main()
